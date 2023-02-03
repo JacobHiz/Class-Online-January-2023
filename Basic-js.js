68da1b1b9fa92208d802778d4751cab1b6edb413
JacobHiz <hiskiajordan@gmail.com>
2023-02-03 23:15:37 +0700

    message

---
+++ Basic-js.js
@@ -0,0 +1,41 @@
+// Variable dan Tipe Data
+
+/**
+ * TIPE DATA
+ * 
+ * Number
+ * String
+ * Boolean
+ * Object
+ * Undefind
+ * Null
+ * /
+
+// Operator ada 2 : Operator Math dan Operator Logic
+
+//Operator Math 
+//+, -, *, /, % (modulus = sisa bagi)
+
+// Operator Logic 
+// ==, !=, >, <, >=, <=,
+"use strict";
+
+var x = 10;
+var y = "true";
+
+var total = x + y;
+// NaN => Not a Number
+
+// console.log(total)
+
+// MODULUS = sisa bagi
+// 7 / 3 = 2 sisa 1
+// 7 % 3 = 1
+// 100 % 7 = 2
+
+// Operator Logic
+var a = 5;
+var b = 5;
+
+var result = a === b;
+console.log(result);
---
+++ index.js
@@ -0,0 +1,71 @@
+// ALGORITMA DAN PSEUDOCODE
+
+// A. Hitung luas segitiga
+
+/* Algoritma 
+ * 1. Rumus luas segitiga 0.5 * alas * tinggi 
+ * 2. Buat variabel alas
+ * 3. Buat variabel tinggi
+ * 4. Buat variabel luas 
+ * 5. Isi variabel luas dengan rumus 0.5 kali alas kali tinggi
+ * 6. Hitung luas segitiga
+ * 7. Cetak luas segitiga
+ *
+ * PSEUDOCODE
+ * 
+ * STORE alas WITH ANY NUMBER
+ * STORE tinggi WITH ANY NUMBER
+ * 
+ * SET luas WITH 0.5 MULTIPLY alas MULTIPLY tinggi 
+ * DISPLAY luas
+ * 
+ * /
+ 
+
+var x = 10;
+var y = 5;
+
+var total = x - y;
+// Nan => Not a Number
+
+console.log(boolean)
+
+
+
+
+
+
+
+var alas = 10;
+var tinggi = 5; 
+
+var luas = 0,5 * alas * tinggi;
+console.log(luas);
+
+// B. Hitung Luas Lingkaran
+
+/**
+ * *Algoritma
+ * 1. Rumus luas lingkaran 3.14 * r * r
+ * 2. Buat variabel r lingkaran
+ * 3. Buat Variabel luas 
+ * 4. Isi variabel luas ke dalam rumus luas lingkaran 3.14 * r * r
+ * 5. Hitung luas lingkaran
+ * 6. Cetak luas lingkaran
+ * 
+ * PSEUDOCODE
+ * 
+ * STORE r WITH ANY NUMBER
+ * STORE r WITH ANY NUMBER
+ * 
+ * SET luas WITH 3.14 MULTIPLY r lingkaran MULTIPLY r
+ * DISPLAY luas
+ * 
+ */
+ 
+
+
+var r = 10
+
+var luas = 3.14 * r * r;
+console.log(luas);
